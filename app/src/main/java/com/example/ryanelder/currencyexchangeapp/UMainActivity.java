c598af0552b6a16c890f328905db9b117cf0b04e
ujjwalnr7 <ujjwalnair_2017@depauw.edu>
2018-02-06 14:04:10 -0500

    Added Java functionality
    Added Java functionality that pulls from the API

--- app/src/main/java/com/example/ryanelder/currencyexchangeapp/UMainActivity.java
+++ app/src/main/java/com/example/ryanelder/currencyexchangeapp/UMainActivity.java
@@ -1,4 +1,4 @@
-package com.ujjwal.currencyconvertor;
+package com.example.ryanelder.currencyexchangeapp;
 
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
