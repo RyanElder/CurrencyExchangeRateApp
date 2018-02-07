af60052d955a8cc5b082c58bf9f6863e7c9200e6
David Elder <delder8@gatech.edu>
2018-02-07 14:02:36 -0500

    Final version

---
+++ app/src/main/java/com/ujjwal/currencyconvertor/FixerService.java
@@ -0,0 +1,12 @@
+package com.ujjwal.currencyconvertor;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface FixerService {
+
+    @GET("latest")
+    Call<Rates> latest(@Query("base") String base);
+
+}
